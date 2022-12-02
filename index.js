37bb38b05b854a4fac1cbbd6ac34670795c6d8c0
artem tutynin <35728337+artm-ttynn@users.noreply.github.com>
2022-12-02 22:29:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const sldrTemp = `
             <div class="sldr__control"></div>
             <div class="sldr__list">
                 <div>
-                    <img data-lazy="https://artm-ttynn.github.io/circle-we-agency/img/image-01.jpg"/>
+                    <img src="https://artm-ttynn.github.io/circle-we-agency/img/image-01.jpg"/>
                 </div>
                 <div>
                     <img data-lazy="https://artm-ttynn.github.io/circle-we-agency/img/image-02.jpg"/>
