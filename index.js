318b2a10226d73b726870e819a68fec4a19b328c
artem tutynin <35728337+artm-ttynn@users.noreply.github.com>
2022-12-02 21:52:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    const block = `t668`;
+    const $wrapper = document.querySelector(`.${block}__textwrapper`)
+    $wrapper.insertAdjacentHTML('beforeend', sldrTemp);
+
+    $('.sldr__list').slick({
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: `linear`,
+        dots: true,
+        appendDots: $(`.sldr__points`),
+        prevArrow: renderArrow(`prev`),
+        nextArrow: renderArrow(`next`),
+        appendArrows: $(`.sldr__control`),
+    });
+});
+
+function renderArrow (position) {
+    return `<button class="arrow ${position}">
+        <div>
+            <svg role="presentation" focusable="false" style="display: block" viewBox="0 0 9.3 17" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <polyline 
+                    fill="none" 
+                    stroke="#222222" 
+                    stroke-linejoin="butt" 
+                    stroke-linecap="butt" 
+                    stroke-width="1" 
+                    points="0.5,0.5 8.5,8.5 0.5,16.5">
+                </polyline>
+            </svg>
+        </div>
+    </button>`;
+};
+
+const sldrTemp = `
+    <div class="sldr__wrapper">
+        <div class="sldr__container">
+            <div class="sldr__control"></div>
+            <div class="sldr__list">
+                <div>
+                    <img data-lazy="https://artm-ttynn.github.io/circle-we-agency/img/image-01.jpg"/>
+                </div>
+                <div>
+                    <img data-lazy="https://artm-ttynn.github.io/circle-we-agency/img/image-02.jpg"/>
+                </div>
+                <div>
+                    <img data-lazy="https://artm-ttynn.github.io/circle-we-agency/img/image-03.jpg"/>
+                </div>
+            </div>
+        </div>
+        <div class="sldr__points"></div>
+    </div>`;
