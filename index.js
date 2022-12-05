7eda6fa248590835b80935e26cab8032fba47908
artem tutynin <35728337+artm-ttynn@users.noreply.github.com>
2022-12-05 16:14:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const $contentBlocks = document.querySelectorAll('.t668__content')
 $contentBlocks.forEach(item => item.style = '')
 
+const $buttons = document.querySelector('.t668__trigger-button')
+$buttons.forEach(($button) => {
+    $button.addEventListener('click', () => {
+        console.log('Клик')
+    })
+})
+
 /*$(document).ready(function(){
     const block = `t668`;
     const $wrapper = document.querySelector(`.${block}__textwrapper`)
